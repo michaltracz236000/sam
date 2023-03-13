353ca5890bb0d124359e031e8afe614d626733d3
Michal Tracz 236000 <236000@edu.p.lodz.pl>
2023-03-13 14:52:50 +0100

    Add video and audio tag

--- lab1/source/server.js
+++ lab1/source/server.js
@@ -3,7 +3,14 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send(req.query.videoFile +" "+req.query.audioFile)
+    var sendTags=""
+    if (req.query.videoFile!=undefined)
+        sendTags+='<video width="500" height="500" controls><source src="'+req.query.videoFile+'">Nie udało się odtworzyc filmu</video>'
+    if (req.query.audioFile!=undefined)
+        sendTags+='<audio controls><source src="'+req.query.audioFile+'">Nie udało się odtworzyć audio</audio>'
+    if(req.query.videoFile==undefined && req.query.audioFile==undefined)
+        sendTags+="<h1>Nothing to show</h1>"
+    res.send(sendTags)
 })
 
 app.listen(4080)
