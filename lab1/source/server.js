abd02c21de8e0dbcce680c1a2ef9b30d29058b3a
Michal Tracz 236000 <236000@edu.p.lodz.pl>
2023-03-13 14:55:20 +0100

    Add id to audio and video tag

--- lab1/source/server.js
+++ lab1/source/server.js
@@ -5,9 +5,9 @@ const app = express()
 app.get('/', (req, res) => {
     var sendTags=""
     if (req.query.videoFile!=undefined)
-        sendTags+='<video width="500" height="500" controls><source src="'+req.query.videoFile+'">Nie udało się odtworzyc filmu</video>'
+        sendTags+='<video width="500" height="500" controls id="videoPlayer"><source src="'+req.query.videoFile+'">Nie udało się odtworzyc filmu</video>'
     if (req.query.audioFile!=undefined)
-        sendTags+='<audio controls><source src="'+req.query.audioFile+'">Nie udało się odtworzyć audio</audio>'
+        sendTags+='<audio controls id="audioPlayer"><source src="'+req.query.audioFile+'">Nie udało się odtworzyć audio</audio>'
     if(req.query.videoFile==undefined && req.query.audioFile==undefined)
         sendTags+="<h1>Nothing to show</h1>"
     res.send(sendTags)
