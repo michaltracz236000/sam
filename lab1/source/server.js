3ea160ce96d6e1819ae4ff6d20c107b2a42799a6
Michal Tracz 236000 <236000@edu.p.lodz.pl>
2023-03-13 15:26:45 +0100

    Change audio and video tags

--- lab1/source/server.js
+++ lab1/source/server.js
@@ -5,11 +5,9 @@ const app = express()
 app.get('/', (req, res) => {
     var sendTags=""
     if (req.query.videoFile!=undefined)
-        sendTags+='<video width="500" height="500" controls id="videoPlayer"><source src="'+req.query.videoFile+'">Nie udało się odtworzyc filmu</video>'
+        sendTags+='<video width="500" height="500" controls id="videoPlayer" src="'+req.query.videoFile+'">Nie udało się odtworzyc filmu</video>'
     if (req.query.audioFile!=undefined)
-        sendTags+='<audio controls id="audioPlayer"><source src="'+req.query.audioFile+'">Nie udało się odtworzyć audio</audio>'
-    if(req.query.videoFile==undefined && req.query.audioFile==undefined)
-        sendTags+="<h1>Nothing to show</h1>"
+        sendTags+='<audio controls id="audioPlayer" src="'+req.query.audioFile+'">Nie udało się odtworzyć audio</audio>'
     res.send(sendTags)
 })
 
