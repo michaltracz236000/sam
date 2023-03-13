989c448d59bdff808044b1b1255ece0e83a51150
Michal Tracz 236000 <236000@edu.p.lodz.pl>
2023-03-13 14:42:42 +0100

    Read params from Get method

--- lab1/source/server.js
+++ lab1/source/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send(req.query.videoFile +" "+req.query.audioFile)
 })
 
 app.listen(4080)
