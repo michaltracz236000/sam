eef8acf495429199b87db19f34752a9ec42c0914
Michal Tracz 236000 <236000@edu.p.lodz.pl>
2023-03-27 14:30:02 +0200

    add img tag

--- lab2/source/server.js
+++ lab2/source/server.js
@@ -8,6 +8,8 @@ app.get('/', (req, res) => {
         sendTags+='<video width="500" height="500" controls id="videoPlayer" src="'+req.query.videoFile+'">Nie udało się odtworzyc filmu</video>'
     if (req.query.audioFile!=undefined)
         sendTags+='<audio controls id="audioPlayer" src="'+req.query.audioFile+'">Nie udało się odtworzyć audio</audio>'
+    if(req.query.imgFile!=undefined)
+        sendTags+='<img id="posterImage" src="'+req.query.imgFile+'">'
     res.send(sendTags)
 })
 
