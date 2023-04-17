d7c3360452a046ef782a21e012be03e3fcc8d4b1
Michal Tracz 236000 <236000@edu.p.lodz.pl>
2023-04-17 14:48:31 +0200

    change show text in buttons

--- lab3/source/server.js
+++ lab3/source/server.js
@@ -20,9 +20,9 @@ app.get('/', (req, res) => {
     </table>
     <br><button id="videoCancel" onclick="videoCancelFun()">videoCancel</button><br>
     <button id="audioCancel" onclick="audioCancelFun()">audioCancel</button><br>
-    <button id="audioAdd" onclick="audioAddFun()">audioAdd</button><br>
-    <button id="videoAdd" onclick="videoAddFun()">videoAdd</button><br>
-    <button id="imgAdd" onclick="imgAddFun()">imgAdd</button><br>
+    <button id="audioAdd" onclick="audioAddFun()">Add audio</button><br>
+    <button id="videoAdd" onclick="videoAddFun()">Add video</button><br>
+    <button id="imgAdd" onclick="imgAddFun()">Add image</button><br>
     <script>
         i=1
         function videoCancelFun()
