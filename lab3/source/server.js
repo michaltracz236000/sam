2e8060546580ad1137d8be9b4b97a2f2ef8ddd56
Michal Tracz 236000 <236000@edu.p.lodz.pl>
2023-04-17 14:22:24 +0200

    replace buttons from lab2

--- lab3/source/server.js
+++ lab3/source/server.js
@@ -3,7 +3,14 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    var sendTags=`<button id="videoCancel" onclick="videoCancelFun()">videoCancel</button><br>
+    var sendTags="";
+    if (req.query.videoFile!=undefined)
+        sendTags+='<video width="500" height="500" controls id="videoPlayer" src="'+req.query.videoFile+'">Nie udało się odtworzyc filmu</video>'
+    if (req.query.audioFile!=undefined)
+        sendTags+='<audio controls id="audioPlayer" src="'+req.query.audioFile+'">Nie udało się odtworzyć audio</audio>'
+    if(req.query.imgFile!=undefined)
+        sendTags+='<img id="posterImage" src="'+req.query.imgFile+'">'
+    sendTags+=`<br><button id="videoCancel" onclick="videoCancelFun()">videoCancel</button><br>
     <button id="audioCancel" onclick="audioCancelFun()">audioCancel</button><br>
     
     <script>
@@ -23,13 +30,7 @@ app.get('/', (req, res) => {
                 audioTag.src="cancel.mp3"
             }
         }
-    </script>`
-    if (req.query.videoFile!=undefined)
-        sendTags+='<video width="500" height="500" controls id="videoPlayer" src="'+req.query.videoFile+'">Nie udało się odtworzyc filmu</video>'
-    if (req.query.audioFile!=undefined)
-        sendTags+='<audio controls id="audioPlayer" src="'+req.query.audioFile+'">Nie udało się odtworzyć audio</audio>'
-    if(req.query.imgFile!=undefined)
-        sendTags+='<img id="posterImage" src="'+req.query.imgFile+'">'
+    </script>`;
     res.send(sendTags)
 })
 
